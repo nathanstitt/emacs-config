5308e7fac97dd147da1e32eb68788bc9d94434aa
Nathan Stitt <nathan@stitt.org>
2015-10-20 09:56:56 -0500

    Turn off lockfiles

--- init.el
+++ init.el
@@ -89,15 +89,15 @@
 (pending-delete-mode t)
 
 ;; make backup to a designated dir, mirroring the full path
-
 (setq
-   backup-by-copying t      ; don't clobber symlinks
-   backup-directory-alist
-    '(("." . "~/.emacs.d/saves"))    ; don't litter my fs tree
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist
+ '(("." . "~/.emacs.d/saves"))    ; don't litter my fs tree
+ delete-old-versions t
+ create-lockfiles nil ; don't litter with .#filename junk, it's a single user system!
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)
 
 
 ; Replace "yes" or "no" resonses with
